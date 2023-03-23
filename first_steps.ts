95d1a1825fd3f9e84b57fb1ff6ae7df88f14cd46
Derek Williams <derek61@gmail.com>
2023-03-23 06:03:06 -0600

    simplest server

--- first_steps.ts
+++ first_steps.ts
@@ -1,4 +1,12 @@
-console.log("Welcome to Deno!");
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
+
+// console.log("Welcome to Deno!");
 
 // const res = await fetch("https://deno.land");
 // const body = await res.text();
