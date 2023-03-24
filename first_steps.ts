20c2bab2ff76ec47d2b8c9fd09a9fd9647b06a98
Derek <derek61@gmail.com>
2023-03-24 12:03:47 -0600

    Update first_steps.ts

--- first_steps.ts
+++ first_steps.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("Hello Mark!", {
+  return new Response("Hello Everyone!", {
     headers: { "content-type": "text/plain" },
   });
 });
